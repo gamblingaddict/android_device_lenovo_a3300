c8d1bff32642693637ab45d9f7dbbd10d4894ab8
gamblingaddict <112048350+gamblingaddict@users.noreply.github.com>
2023-12-16 07:55:37 +0700

    a3300: Remove Go config
    This was causing issues.

--- device.mk
+++ device.mk
@@ -4,7 +4,6 @@ VENDOR_PATH := vendor/lenovo/a3300
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 
 # Firmware
 $(call inherit-product, $(VENDOR_PATH)/vendor.mk)
